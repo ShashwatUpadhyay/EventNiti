d48db576122f12a921ed56c5c8612c34e1c21e5e
ShashwatUpadhyay <shashwatup619@gmail.com>
2025-02-04 17:57:16 +0530

    changes

--- base/admin.py
+++ base/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+from .models import *
 
 admin.site.site_header = "Prerogative Pointers Administration"  
 admin.site.site_title = "Prerogative Pointers Admin Portal"     
@@ -7,3 +7,4 @@ admin.site.index_title = "Welcome to Prerogative Pointers Admin Panel"
 
 # Register your models here.
 
+admin.site.register(Review)
