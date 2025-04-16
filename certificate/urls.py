53e3502b9fc5185a1fa95a6e0c8a531a36c42a02
ShashwatUpadhyay <shashwatup619@gmail.com>
2025-04-17 01:27:10 +0530

    removed pdf generator

--- certificate/urls.py
+++ certificate/urls.py
@@ -3,5 +3,4 @@ from . import views
 urlpatterns = [
     path('', views.certificates, name='certificates'),
     path('<hash>', views.certificate, name='certificate'),
-    path('pdf/<hash>', views.certificate_generate_pdf, name='certificate_generate_pdf')
 ]
--- certificate/views.py
+++ certificate/views.py
@@ -9,45 +9,9 @@ from ppuu import settings
 from ppuu.settings import DOMAIN_NAME
 from datetime import datetime
 from django.template.loader import render_to_string
-import pdfkit
 import base64
 import os
-path = 'wkhtmltopdf.exe'
 
-# Create your views here.
-config = pdfkit.configuration(wkhtmltopdf=path)
-# config = pdfkit.configuration(wkhtmltopdf=r"C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe")
-
-def certificate_generate_pdf(request,hash):
-    cert  = models.Certificate.objects.get(hash=hash)
-
-    # Render the HTML page
-    html_string = render_to_string("certificate.html", {'certi_obj':cert,'head':None,'linkedin_url':None,'qr_code_base64': generate_qr_code_base64(f'{settings.DOMAIN_NAME}certificate/{cert.hash}')})
-
-    # Make sure all static URLs are absolute
-    html_string = html_string.replace('src="/', f'src="{settings.DOMAIN_NAME}/')
-    html_string = html_string.replace('href="/', f'href="{settings.DOMAIN_NAME}/')
-
-    # Convert HTML to PDF
-    options = {
-    'margin-top': '0',
-    'margin-right': '0',
-    'margin-bottom': '0',
-    'margin-left': '0',
-    'disable-smart-shrinking': '',
-    'page-size': 'Letter',
-    'encoding': "UTF-8",
-    'page-size': 'A4',
-    'custom-header': [('Accept-Encoding', 'gzip')],
-    'no-outline': None,
-    }
-    pdf = pdfkit.from_string(html_string, False,configuration=config, options=options)
-
-    # Send response as a PDF file in the browser
-    response = HttpResponse(pdf, content_type="application/pdf")
-    response["Content-Disposition"] = f"inline; filename={cert.user.get_full_name}_certificate.pdf"  # Opens in browser
-
-    return response
 
 def generate_certificate_link(student,certif_name, certi, dt):
     base_url = f"https://www.linkedin.com/profile/add?startTask={certif_name}"
