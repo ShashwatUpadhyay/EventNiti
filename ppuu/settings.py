cbbc71856cebd70010f2c95bcb90360e90cf7d4a
ShashwatUpadhyay <shashwatup619@gmail.com>
2025-09-02 19:04:26 +0530

    logger added

--- ppuu/settings.py
+++ ppuu/settings.py
@@ -204,3 +204,55 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 RAZOR_KEY_ID = env("RAZOR_KEY_ID")
 RAZOR_KEY_SECRET = env("RAZOR_KEY_SECRET")
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+
+    'formatters': {
+        'verbose': {
+            'format': '[{levelname}] {asctime} {name} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
+
+    'handlers': {
+        'file_error': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/error.log'),
+            'formatter': 'verbose',
+        },
+        'file_server': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/server.log'),
+            'formatter': 'verbose',
+        },
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+
+    'root': {
+        'handlers': ['console', 'file_error'],
+        'level': 'WARNING',
+    },
+
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'file_server'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        'django.request': {   # logs unhandled exceptions
+            'handlers': ['file_error'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    },
+}
